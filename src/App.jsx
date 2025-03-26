c0dfdffad203091dbf40a8b2fe7efc03f8eebe16
smitkapadiya07 <smitkapadiya07@gmail.com>
2025-03-26 18:57:08 +0530

    app file updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,27 @@ import './App.css'
 import Navigation from "./components/global/navigation.jsx";
 import Home from "./pages/homepage/home.jsx";
 import Footer from "./components/global/footer.jsx";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import About from "./pages/Aboutpage/about.jsx";
 import Engagement from "./pages/Engagement/Engagement.jsx";
 import Resource from "./pages/resource/resource.jsx";
 import Contact from "./pages/Contact/contact.jsx";
+import {useEffect} from "react";
 
 function App() {
+
+    function ScrollToTop() {
+        const {pathname} = useLocation();
+        useEffect(() => {
+            window.scrollTo(0, 0);
+        }, [pathname]);
+        return null;
+    }
 
     return (
         <>
             <Navigation/>
-
+            <ScrollToTop/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/about" element={<About/>}/>
