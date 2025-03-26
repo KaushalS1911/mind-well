f6b93009ae596688f8583e9ec3c7d56225cd7823
KaushalS1911 <kaushalsojitra19@gmail.com>
2025-03-26 14:10:32 +0530

    engagement framework

--- src/pages/homepage/home.jsx
+++ src/pages/homepage/home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSection from "../../components/Home/herosection.jsx";
 import Mindwell from "../../components/Home/mindwell.jsx";
 import WorksTable from "../../components/Home/worksTable.jsx";
+import EngagementFramework from "../../components/Home/EngagementFramework.jsx";
 
 function Home() {
     return (
@@ -9,6 +10,7 @@ function Home() {
             <HeroSection/>
             <Mindwell/>
             <WorksTable/>
+            <EngagementFramework />
         </>
     );
 }
