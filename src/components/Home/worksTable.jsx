c2599c32cf6505c290afa5d77042d15294462b1c
smitkapadiya07 <smitkapadiya07@gmail.com>
2025-04-30 10:21:04 +0530

    other changing

--- src/components/Home/worksTable.jsx
+++ src/components/Home/worksTable.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import {Box, Container, Grid, Typography} from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CircleIcon from '@mui/icons-material/Circle';
 import img1 from "../../assets/images/Home/workstable/Establish-Leadership.jpeg";
@@ -58,187 +58,191 @@ const steps = [
 
 function WorksTable() {
     return (
-        <Container fixed>
-            <Box sx={{ padding: "0" }}>
-                <Typography
-                    className="Montserrat"
-                    variant="h4"
-                    align="center"
-                    fontWeight={700}
-                    color="#012765"
-                    gutterBottom
-                    sx={{
-                        fontSize: { xs: '28px', sm: '34px', md: '42px', lg: '46px' },
-                        letterSpacing: "-1px",
-                    }}
-                >
-                    How we Make a Difference
-                </Typography>
-
-                <Box
-                    align="center"
-                    sx={{
-                        mx: { xs: 2, sm: 6, md: 12 },
-                        color: "#4B5563",
-                        fontSize: { xs: 18, sm: 20 },
-                        lineHeight: 1.75,
-                    }}
-                >
-                    With EmotionallYours, you can embark on a transformative journey to
-                    enhance your emotional well-being through a few simple and effective
-                    steps.
-                </Box>
-
-                <Box sx={{ position: "relative", mt: { xs: 4, sm: 6 } }}>
-                    <Box
+        <Box sx={{
+            px: {sm:"50px ",xs:"0",xl:"0"},
+        }}>
+            <Container maxWidth={"xl"}>
+                <Box sx={{padding: "0"}}>
+                    <Typography
+                        className="Montserrat"
+                        variant="h4"
+                        align="center"
+                        fontWeight={700}
+                        color="#012765"
+                        gutterBottom
                         sx={{
-                            position: "absolute",
-                            left: 0,
-                            top: 0,
-                            bottom: 0,
-                            width: { md: "4px", xs: 0 },
-                            bgcolor: "#f3e2d3",
-                            transform: "translateX(-50%)",
-                            zIndex: 0,
+                            fontSize: {xs: '28px', sm: '34px', md: '42px', lg: '46px'},
+                            letterSpacing: "-1px",
                         }}
-                    />
+                    >
+                        How we Make a Difference
+                    </Typography>
 
-                    {steps.map((step, index) => (
+                    <Box
+                        align="center"
+                        sx={{
+                            mx: {xs: 2, sm: 6, md: 12},
+                            color: "#4B5563",
+                            fontSize: {xs: 18, sm: 20},
+                            lineHeight: 1.75,
+                        }}
+                    >
+                        With EmotionallYours, you can embark on a transformative journey to
+                        enhance your emotional well-being through a few simple and effective
+                        steps.
+                    </Box>
+
+                    <Box sx={{position: "relative", mt: {xs: 4, sm: 6}}}>
                         <Box
-                            key={index}
                             sx={{
-                                position: "relative",
-                                mt: index === 0 ? 0 : { xs: 4 },
+                                position: "absolute",
+                                left: 0,
+                                top: 0,
+                                bottom: 0,
+                                width: {md: "4px", xs: 0},
+                                bgcolor: "#f3e2d3",
+                                transform: "translateX(-50%)",
+                                zIndex: 0,
                             }}
-                        >
-                            <Box
-                                sx={{
-                                    position: "absolute",
-                                    top: { lg: "50%", md: "50%", sm: "50%" },
-                                    left: { lg: "-2.1%", md: "-2.8%", sm: "-4.8%" },
-                                    display: { xs: "none", sm: "inline-flex" },
-                                    alignItems: "center",
-                                    justifyContent: "center",
-                                    width: 50,
-                                    height: 50,
-                                    borderRadius: "50%",
-                                    bgcolor: "#FE6A00",
-                                    color: "#fff",
-                                    fontWeight: "bold",
-                                    fontSize: 18,
-                                    transform: "translateY(-50%)",
-                                    zIndex: 1,
-                                }}
-                            >
-                                <CircleIcon />
-                            </Box>
+                        />
 
+                        {steps.map((step, index) => (
                             <Box
+                                key={index}
                                 sx={{
-                                    display: "flex",
-                                    flexDirection: { xs: "column", md: "row" },
-                                    bgcolor: "#fff",
-                                    borderRadius: 2,
-                                    p: { xs: 3, sm: 3, md: 4 },
-                                    border: "1px solid #E5E7EB",
-                                    gap: 4,
-                                    ml: { md: 5, xs: 0 },
                                     position: "relative",
-                                    transition: "0.3s",
-                                    "&:hover": {
-                                        boxShadow: "0 4px 20px rgba(0,0,0,0.2)",
-                                    },
+                                    mt: index === 0 ? 0 : {xs: 4},
                                 }}
                             >
                                 <Box
                                     sx={{
                                         position: "absolute",
-                                        top: "10px",
-                                        right: "12px",
-                                        display: { xs: "inline-flex", sm: "none" },
+                                        top: {lg: "50%", md: "50%", sm: "50%"},
+                                        left: {lg: "-2.1%", md: "-2.8%", sm: "-4.8%"},
+                                        display: {xs: "none", sm: "inline-flex"},
                                         alignItems: "center",
                                         justifyContent: "center",
-                                        width: 35,
-                                        height: 35,
+                                        width: 50,
+                                        height: 50,
                                         borderRadius: "50%",
                                         bgcolor: "#FE6A00",
                                         color: "#fff",
                                         fontWeight: "bold",
-                                        fontSize: 14,
+                                        fontSize: 18,
+                                        transform: "translateY(-50%)",
+                                        zIndex: 1,
                                     }}
                                 >
-                                    <CircleIcon />
+                                    <CircleIcon/>
                                 </Box>
 
-                                <Grid container spacing={4}>
-                                    <Grid
-                                        item
-                                        xs={12}
-                                        sm={7}
-                                        display="flex"
-                                        flexDirection="column"
-                                        justifyContent="center"
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        flexDirection: {xs: "column", md: "row"},
+                                        bgcolor: "#fff",
+                                        borderRadius: 2,
+                                        p: {xs: 3, sm: 3, md: 4},
+                                        border: "1px solid #E5E7EB",
+                                        gap: 4,
+                                        ml: {md: 5, xs: 0},
+                                        position: "relative",
+                                        transition: "0.3s",
+                                        "&:hover": {
+                                            boxShadow: "0 4px 20px rgba(0,0,0,0.2)",
+                                        },
+                                    }}
+                                >
+                                    <Box
+                                        sx={{
+                                            position: "absolute",
+                                            top: "10px",
+                                            right: "12px",
+                                            display: {xs: "inline-flex", sm: "none"},
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                            width: 35,
+                                            height: 35,
+                                            borderRadius: "50%",
+                                            bgcolor: "#FE6A00",
+                                            color: "#fff",
+                                            fontWeight: "bold",
+                                            fontSize: 14,
+                                        }}
                                     >
-                                        <Typography
-                                            sx={{
-                                                fontWeight: 700,
-                                                fontSize: { xs: 23, sm: 25, md: 28 },
-                                                mb: 2,
-                                                color: "#002F6C",
-                                            }}
-                                        >
-                                            {step.title}
-                                        </Typography>
+                                        <CircleIcon/>
+                                    </Box>
 
-                                        {step.details.map((detail, i) => (
-                                            <Box key={i} display="flex" mb={1}>
-                                                <CheckCircleIcon
-                                                    sx={{ color: "#FE6A00", mr: 1, mt: 0.5, fontSize: 20 }}
-                                                />
-                                                <Typography
-                                                    sx={{ fontSize: { xs: 14, sm: 18 }, lineHeight: 1.5 }}
-                                                >
-                                                    {detail}
-                                                </Typography>
-                                            </Box>
-                                        ))}
-                                    </Grid>
-
-                                    <Grid
-                                        item
-                                        xs={12}
-                                        sm={5}
-                                        display="flex"
-                                        alignItems="center"
-                                        justifyContent="center"
-                                    >
-                                        <Box
-                                            sx={{
-                                                height: "220px",
-                                                width: "400px",
-                                            }}
+                                    <Grid container spacing={4}>
+                                        <Grid
+                                            item
+                                            xs={12}
+                                            sm={7}
+                                            display="flex"
+                                            flexDirection="column"
+                                            justifyContent="center"
                                         >
-                                            <img
-                                                src={step.image}
-                                                alt={step.title}
-                                                style={{
-                                                    width: "100%",
-                                                    height: "100%",
-                                                    objectFit: "contain",
-                                                    borderRadius: 8,
-                                                    transition: "transform 0.3s ease-in-out",
+                                            <Typography
+                                                sx={{
+                                                    fontWeight: 700,
+                                                    fontSize: {xs: 23, sm: 25, md: 28},
+                                                    mb: 2,
+                                                    color: "#002F6C",
                                                 }}
-                                                className="hover-scale-image"
-                                            />
-                                        </Box>
+                                            >
+                                                {step.title}
+                                            </Typography>
+
+                                            {step.details.map((detail, i) => (
+                                                <Box key={i} display="flex" mb={1}>
+                                                    <CheckCircleIcon
+                                                        sx={{color: "#FE6A00", mr: 1, mt: 0.5, fontSize: 20}}
+                                                    />
+                                                    <Typography
+                                                        sx={{fontSize: {xs: 14, sm: 18}, lineHeight: 1.5}}
+                                                    >
+                                                        {detail}
+                                                    </Typography>
+                                                </Box>
+                                            ))}
+                                        </Grid>
+
+                                        <Grid
+                                            item
+                                            xs={12}
+                                            sm={5}
+                                            display="flex"
+                                            alignItems="center"
+                                            justifyContent="center"
+                                        >
+                                            <Box
+                                                sx={{
+                                                    height: "220px",
+                                                    width: "400px",
+                                                }}
+                                            >
+                                                <img
+                                                    src={step.image}
+                                                    alt={step.title}
+                                                    style={{
+                                                        width: "100%",
+                                                        height: "100%",
+                                                        objectFit: "contain",
+                                                        borderRadius: 8,
+                                                        transition: "transform 0.3s ease-in-out",
+                                                    }}
+                                                    className="hover-scale-image"
+                                                />
+                                            </Box>
+                                        </Grid>
                                     </Grid>
-                                </Grid>
+                                </Box>
                             </Box>
-                        </Box>
-                    ))}
+                        ))}
+                    </Box>
                 </Box>
-            </Box>
-        </Container>
+            </Container>
+        </Box>
     );
 }
 
